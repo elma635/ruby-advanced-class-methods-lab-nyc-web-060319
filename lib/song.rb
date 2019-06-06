19583c3dc49461b887605f4063f24d8fa9a7042a
Elma Rahman <elmarahman635@gmail.com>
2019-06-05 21:12:39 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,53 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+def initialize(name, artist_name)
+  @name = name
+  @artist_name = artist_name
+
+  @@all << self
+end
+
+
+
   def self.all
     @@all
   end
 
+#HELP ON  THIS  METHOD
+  def self.create
+    song = self.new
+    song.name = name
+    @@all << song
+  end
+
+#HELP
+  def self.create_by_name(name)
+    @@all.find do |song|
+      song.create.name = name
+    end
+  end
+
+#DONE
+  def self.find_by_name(name)
+    @@all.find do |song| song.name == name
+    end
+  end
+
+
+
+#HELP
+  def self.new_by_name(name)
+    @@all.map do |song|
+      song.name = name
+    end
+  end
+
+
+
   def save
     self.class.all << self
   end
+
 
 end
